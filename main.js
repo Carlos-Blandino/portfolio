d3c6a66ed8f9a8db4205afee141c3bb9ab4635e2
Carlos Blandino <cablandino66@gmail.com>
2021-02-26 09:34:29 -0800

    fixing html typos

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const localLibraryButton = document.querySelector("#localLibrary");
 const decoderRingButton = document.querySelector("#decoderRing");
 
 localLibraryButton.addEventListener("click", (event) => {
+    event.preventDefault();
     window.open("https://carlos-blandino.github.io/local-library-project/", "_blank")
 });
 decoderRingButton.addEventListener("click", (event) => {
+    event.preventDefault();
     window.open("https://carlos-blandino.github.io/decoder-ring-project/", "_blank")
 })
