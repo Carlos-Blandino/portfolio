14384e18ee353a654f230f0989b888204a89c961
Carlos Blandino <cablandino66@gmail.com>
2021-04-20 08:28:24 -0700

    adding to portfolio, pomodoro

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const movieProjectButton = document.querySelector("#movieProject");
 const decoderRingButton = document.querySelector("#decoderRing");
+const pomodoroProjectButton = document.querySelector("#pomodoroProject")
 
 movieProjectButton.addEventListener("click", (event) => {
     event.preventDefault();
     window.open("https://starter-movie-front-end-mocha.vercel.app/", "_blank")
+});
+pomodoroProjectButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.open("https://project-pomodoro-timer-qualified-1-three.vercel.app/", "_blank")
 });
 decoderRingButton.addEventListener("click", (event) => {
     event.preventDefault();
