0ee73b1ce44f490ce5ffdad7d094c51f3d7bfdce
Carlos Blandino <cablandino66@gmail.com>
2021-05-26 22:14:11 -0700

    Adding new project

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const movieProjectButton = document.querySelector("#movieProject");
 const decoderRingButton = document.querySelector("#decoderRing");
 const pomodoroProjectButton = document.querySelector("#pomodoroProject")
 const grubDashProject = document.querySelector("#grubDashProject");
-
+const reservationProject = document.querySelector("#reservationProject");
 grubDashProject.addEventListener("click", (event) => {
     event.preventDefault();
     window.open("https://starter-grub-dash-front-end-plum.vercel.app/", "_blank")
@@ -19,4 +19,8 @@ pomodoroProjectButton.addEventListener("click", (event) => {
 decoderRingButton.addEventListener("click", (event) => {
     event.preventDefault();
     window.open("https://carlos-blandino.github.io/decoder-ring-project/", "_blank")
+})
+reservationProject.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.open("https://frontend-restaurant-reservation.vercel.app/dashboard", "_blank")
 })
